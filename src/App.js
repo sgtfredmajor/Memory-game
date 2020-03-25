2323c73fe3f14414a5d6a5fd87ed487790de22a7
Fred Major <sgtfredmajor@gmail.com>
2020-03-25 14:45:08 -0400

    Completed step1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import React from 'react'
+=======
+import React from 'react';
+>>>>>>> 504003293f315ae9d317c7859872f05ea64fce18
 import './App.css';
 import MemoryCard from "./components/MemoryCard";
 
@@ -6,8 +10,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+<<<<<<< HEAD
       <h1>Memory Game</h1>
       <h3>Match cards to win</h3>
+=======
+
+        <h1>
+          Memory Game
+        </h1>
+        <h3>
+          Match cards win
+        </h3>
+        <MemoryCard />
+>>>>>>> 504003293f315ae9d317c7859872f05ea64fce18
       </header>
       <div>
       <MemoryCard />
--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -6,7 +6,11 @@ class MemoryCard extends Component {
   render() {
     return (
         <div className="MemoryCard">
+<<<<<<< HEAD
             <img src= { image } alt="Digital Crafts logo" /> 
+=======
+            <img src= { image } alt="Digital Crafts logo" />
+>>>>>>> 504003293f315ae9d317c7859872f05ea64fce18
         </div>
     );
   }
