f2674dda74d01557ae6b7d42c260007b33121314
Fred Major <sgtfredmajor@gmail.com>
2020-03-25 14:27:12 -0400

    completed step 1

---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import './MemoryCard.css';
+import image from './digitalcrafts-logo-white-y.png';
+
+class MemoryCard extends Component {
+  render() {
+    return (
+        <div className="MemoryCard">
+            <img src= { image } alt="Digital Crafts logo" /> 
+        </div>
+    );
+  }
+}
+
+export default MemoryCard;
