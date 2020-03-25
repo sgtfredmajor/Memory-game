811dcd8c2283e7ab672d6c00c25faae8bca66e29
Fred Major <sgtfredmajor@gmail.com>
2020-03-25 15:08:53 -0400

    Corrected completed step1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-<<<<<<< HEAD
-import React from 'react'
-=======
 import React from 'react';
->>>>>>> 504003293f315ae9d317c7859872f05ea64fce18
 import './App.css';
-import MemoryCard from "./components/MemoryCard";
+import MemoryCard from './components/MemoryCard';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
       <h1>Memory Game</h1>
       <h3>Match cards to win</h3>
-=======
-
-        <h1>
-          Memory Game
-        </h1>
-        <h3>
-          Match cards win
-        </h3>
-        <MemoryCard />
->>>>>>> 504003293f315ae9d317c7859872f05ea64fce18
       </header>
       <div>
       <MemoryCard />
